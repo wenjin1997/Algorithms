c75ea777e14e60cea71804cbbe218cbbddea6497
wenjin1997 <wenjin1997@126.com>
2021-10-15 20:13:54 +0800

    增加 链表实现背包、队列及栈代码

--- Algorithms/src/chapter01/bag/Bag.java
+++ Algorithms/src/chapter01/bag/Bag.java
@@ -8,25 +8,48 @@ import java.util.function.Consumer;
  * @version 1.0
  */
 public class Bag<Item> implements Iterable<Item>{
-    Bag(){
-
+    private Node first; // 链表的首结点
+    private class Node{
+        Item item;
+        Node next;
     }
-    void add(Item item) {
-
-    }
-
-    boolean isEmpty(){
-        return true;
-    }
-
-    int size(){
-        return 1;
+    public void add(Item item){
+        // 和Stack的push()方法完全相同
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
     }
 
 
     @Override
     public Iterator<Item> iterator() {
         return null;
+    }
+
+    private class ListIterator implements Iterator<Item>{
+        private Node current = first;
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        @Override
+        public void remove() {
+            Iterator.super.remove();
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super Item> action) {
+            Iterator.super.forEachRemaining(action);
+        }
     }
 
     @Override
